c37a7f9f5d474368f4c6c6c98d30031a679a2d63
NexusPrime <nicolasestevaolima104@gmail.com>
2023-10-13 01:11:36 -0300

    changes on modal

--- src/components/StartModal.tsx
+++ src/components/StartModal.tsx
@@ -11,19 +11,11 @@ function StartModal() {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-70 z-50 ${
+      className={`fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-60 z-[60] ${
         isOpen ? 'block' : 'hidden'
       }`}
     >
-      <div className="bg-white p-6 rounded-lg text-center">
-        <p className="text-lg font-semibold mb-4">Click here to start</p>
-        <button
-          onClick={closeModal}
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-        >
-          Close
-        </button>
-      </div>
+        <p className="text-2xl font-semibold mb-4 text-white">Click here to start</p>
     </div>
   );
 }
