e025c2fa4f28b6f62f5b100e8ba0e226cf86f5dc
nexus <nicolas.estevao.lima1004@hotmail.com>
2023-10-15 01:00:34 -0300

    added touch event to modal

--- src/components/StartModal.tsx
+++ src/components/StartModal.tsx
@@ -16,6 +16,7 @@ function StartModal() {
 		data-open={isOpen}
 		className='fixed top-0 left-0 w-full h-full data-[open=false]:hidden flex justify-center items-center bg-black bg-opacity-95 backdrop-blur-md z-[60]'
 		onClick={closeModal}
+		onTouchEnd={closeModal}
 		id='start-modal'
     >
         <p className="text-3xl mb-4 text-white">Click here to start</p>
